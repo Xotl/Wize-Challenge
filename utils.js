6b573e320381e5d83126e20333d8ebe9e3eea49a
Edgardo Campos (Xotl) <edgardo333@gmail.com>
2016-07-09 22:04:50 -0500

    Commenting bad code on utils

--- utils.js
+++ utils.js
@@ -44,40 +44,40 @@ module.exports = {
       })
     } )
 
-  },
-
-  createConnectionString: conf => {
-    var login = ''
-      
-    if (conf.user && conf.password) {
-      login = conf.user + ':' + conf.password + '@'
-    }
-
-    if (!Array.isArray(conf.servers) || conf.servers.length < 1) {
-      console.error('=> Must provide at least one Database server');
-      process.exit(1);
-    }
-
-    if (typeof conf.database !== "string" || conf.database.length === 0) {
-      console.error('=> Database name should be nonempty string');
-      process.exit(1);
-    }
-
-    const
-      database = conf.database,
-      hosts = conf.servers.reduce((str, server, index) => {
-
-        if (index === 1) {
-          str = `${str}`
-          
-          if (!server) {
-            return
-          } 
-        }
-
-        return `${str},${}`
-      }, '')
-
-    return `mongodb://${login}${hosts}/${database}`;
   }
+
+  // createConnectionString: conf => {
+  //   var login = ''
+      
+  //   if (conf.user && conf.password) {
+  //     login = conf.user + ':' + conf.password + '@'
+  //   }
+
+  //   if (!Array.isArray(conf.servers) || conf.servers.length < 1) {
+  //     console.error('=> Must provide at least one Database server');
+  //     process.exit(1);
+  //   }
+
+  //   if (typeof conf.database !== "string" || conf.database.length === 0) {
+  //     console.error('=> Database name should be nonempty string');
+  //     process.exit(1);
+  //   }
+
+  //   const
+  //     database = conf.database,
+  //     hosts = conf.servers.reduce((str, server, index) => {
+
+  //       if (index === 1) {
+  //         str = `${str}`
+          
+  //         if (!server) {
+  //           return
+  //         } 
+  //       }
+
+  //       return `${str},${}`
+  //     }, '')
+
+  //   return `mongodb://${login}${hosts}/${database}`;
+  // }
 }
